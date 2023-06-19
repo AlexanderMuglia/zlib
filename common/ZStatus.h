ca9cc18597d81feb8c01dbc0995e5fa4e76d664a
alex <alexander.muglia@hotmail.com>
2023-06-18 22:05:55 -0400

    Introduced status functionality... with different status numbers...

--- common/ZStatus.h
+++ common/ZStatus.h
@@ -3,8 +3,8 @@
 typedef     uint32_t          ZSTATUS;
 
 #define             ZSTATUS_OK                        0x00000000
-#define             ZSTATUS_FAILED                    0x00000000
-#define             ZSTATUS_INVALID_ARGS              0x00000000
-#define             ZSTATUS_OUT_OF_MEMORY             0x00000000
-#define             ZSTATUS_ERR_INTERNAL              0x00000000
+#define             ZSTATUS_FAILED                    0x00000001
+#define             ZSTATUS_INVALID_ARGS              0x00000002
+#define             ZSTATUS_OUT_OF_MEMORY             0x00000003
+#define             ZSTATUS_ERR_INTERNAL              0x00000004
 
