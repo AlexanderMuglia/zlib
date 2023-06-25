c07b26d5e8ba852d65a86fce59f540872542e60a
alex <alexander.muglia@hotmail.com>
2023-06-24 23:29:37 -0400

    Setting up bubblesort

---
+++ libs/algorithms/sort/bubble_sort.c
@@ -0,0 +1,23 @@
+int
+ZBubbleSort
+(int*  array)
+{
+
+    return 0;
+}
+
+int main ()
+{
+    //ZSTATUS     status      =  ZSTATUS_OK;
+    int         array[4];
+
+    array[0] = 3;
+    array[1] = -4;
+    array[2] = 0;
+    array[3] = 1;
+
+    (void) ZBubbleSort( array );
+
+
+    return 0;
+}
