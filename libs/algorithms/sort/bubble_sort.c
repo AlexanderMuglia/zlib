bf45f0723874db926601a2905270da78928471a9
alex <alexander.muglia@hotmail.com>
2023-06-25 13:40:26 -0400

    Bubble sort

--- libs/algorithms/sort/bubble_sort.c
+++ libs/algorithms/sort/bubble_sort.c
@@ -1,14 +1,42 @@
-int
-ZBubbleSort
-(int*  array)
-{
+#include "include/bubble_sort.h"
 
-    return 0;
+ZSTATUS
+ZBubbleSort
+(
+    int*    Array,
+    int     ArrayLen
+)
+{
+    ZSTATUS         status      =  ZSTATUS_OK;
+    int             i           =  0;
+    int             j           =  0;
+
+    if( Array )
+    {
+        for( i = 0; i < ArrayLen - 1; i++ )
+        {
+            for( j = 0; j < ArrayLen - i - 1; j++ )
+            {
+                if( Array[i] > Array[i + 1] )
+                {
+                    Array[i] = Array[i] ^ Array[i + 1];
+                    Array[i + 1 ] = Array[i] ^ Array[i + 1];
+                    Array[i] = Array[i] ^ Array[i + 1];
+                }
+            }
+        }
+    }
+    else
+    {
+        status = ZSTATUS_INVALID_ARGS;
+    }
+
+    return status;
 }
 
 int main ()
 {
-    //ZSTATUS     status      =  ZSTATUS_OK;
+    ZSTATUS     status      =  ZSTATUS_OK;
     int         array[4];
 
     array[0] = 3;
@@ -16,8 +44,19 @@ int main ()
     array[2] = 0;
     array[3] = 1;
 
-    (void) ZBubbleSort( array );
+    printf("Before sort: \n");
+    for( int i = 0; i < 4; i++ )
+    {
+        printf("%d\n", array[i]);
+    }
 
+    status = ZBubbleSort( array, 4 );
+
+    printf("After sort: \n");
+    for( int i = 0; i < 4; i++ )
+    {
+        printf("%d\n", array[i]);
+    }
 
     return 0;
 }
